5d290b58fc11af58d4122bbca6e1fd39ed6368b2
victorpalominoborg <victor.palomino@softwareandina.com>
2018-01-09 11:53:56 -0500

    mantemiento de clientes, producto, vendedor, registro de facturas, reportes

---
+++ Customers.cs
@@ -0,0 +1,9 @@
+namespace VisualCode.Entities
+{
+    public class Customers
+    {
+        public int IdCustomer { get; set; }
+        public string Name { get; set; }
+        public string BirthDate { get; set; }
+    }
+}
