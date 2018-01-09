91b33a4c6f0e7ea72127f5932dc85f98562d42f0
victorpalominoborg <victor.palomino@softwareandina.com>
2018-01-09 14:57:53 -0500

    Corrected and applied good practices, optimization, refactorización use of words in English in the sales record.

---
+++ Products.cs
@@ -0,0 +1,15 @@
+namespace VisualCode.Entities
+{
+    public class Products
+    {
+        public int IdProduct { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(this.Description)}: {this.Description}: {this.Description} {this.Price}";
+        }
+    }
+}
